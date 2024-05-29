e17c0476eac6c1dfad34e6f08a97bab9f114c4fc
Notyasser <showhad55@gmail.com>
2024-05-29 21:29:37 +0200

    add files

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,5 @@
-export default function getFullResponseFromAPI(success) 
+/* eslint-disable */
+export default function getResponseFromAPI() 
 {
-    return new Promise((resolve, reject) => {
-      if (success) {
-        resolve({ status: 200, body: 'Success' });
-      } else {
-        reject(new Error('The fake API is not working currently'));
-      }
-    });
+    return (new Promise((resolve, reject) => {}));
 }
